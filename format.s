78fd24df971ae49de74f96bd19e98517a80bfac5
Peter Fidelman <edlinfan@defiant>
2014-12-03 22:18:21 -0800

    Initial commit
    Got an initial draft of the song format.  It is a NASM file that you
    can assemble down to a playable song.  This will allow me to write
    the player before I have a working editor.

---
+++ format.s
@@ -0,0 +1,91 @@
+Magic:			db      'X'
+
+Version:		db	00h
+
+Artist:			db      'P','e','t','e','r',' ','F','i'
+			db      'd','e','l','m','a','n',' ',' '
+
+Title:			db      'T','e','s','t',' ','s','o','n'
+			db      'g',' ',' ',' ',' ',' ',' ',' '
+
+StartOfInstruments:	dw      Instruments
+
+StartOfSpFx:		dw	SpFx
+
+StartOfOrders:		dw      Orders
+
+StartOfPatterns:	dw      Patterns
+
+Instruments:		db      0FFh,0Fh,00h,00h,00h,00h,00h,00h,00h,00h,00h,00h
+			db	'T','e','s','t',' ','i','n','s','t','r','u','m','e','n','t',' '
+
+SpFx:			; TODO
+
+Orders:			db	00h,01h,01h,01h,01h,01h,01h,01h,01h
+
+Patterns:		db	00h			;0
+			db	20h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h			;1
+			db	22h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
+			db	00h,00h,00h,00h,00h
